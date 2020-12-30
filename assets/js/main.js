c912402eda946d8e54ae49761df5cec6ea026e87
OmarAliSoliman <omarasmar9955@gmail.com>
2020-12-30 17:00:35 +0200

    first

---
+++ assets/js/main.js
@@ -0,0 +1,102 @@
+$(document).ready(function () {
+  "use strict";
+
+  $('.project-details-slider').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+  });
+
+  $('#project-slider-1').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: false,
+    asNavFor: '#pro-det-slider-1',
+    autoplay: false,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+
+  $('#pro-det-slider-1').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '#project-slider-1',
+    dots: false,
+    fade: true,
+    centerMode: true,
+    autoplay: true,
+    infinite: true,
+  });
+
+
+  $('#project-slider-2').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: false,
+    asNavFor: '#pro-det-slider-2',
+    autoplay: false,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+
+  $('#pro-det-slider-2').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '#project-slider-2',
+    dots: false,
+    fade: true,
+    centerMode: true,
+    autoplay: true,
+    infinite: true,
+  });
+});
